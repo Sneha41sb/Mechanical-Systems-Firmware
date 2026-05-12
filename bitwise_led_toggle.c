bcd0a360b4bc12c0d357c532f608a0150115172e
Sneha S Biju <snehasbiju2004@gmail.com>
2026-05-12 23:53:56 +0530

    Implement LED toggle based on sensor state
    Added functionality to toggle LED based on sensor input.

--- bitwise_led_toggle.c
+++ bitwise_led_toggle.c
@@ -1,11 +1,25 @@
-//xor
 void setup() {
+  // Address for Output Enable
   volatile uint32_t *gpio_out_en_reg = (volatile uint32_t *)0x3FF44020;
-  *gpio_out_en_reg |= (1 << 2); 
+  // Set Pin 2 (LED) as OUTPUT
+  *gpio_out_en_reg |= (1 << 2);
+  // Pin 4 is INPUT by default
 }
 
 void loop() {
+  // Input Register (Reading the world)
+  volatile uint32_t *gpio_in_reg = (volatile uint32_t *)0x3FF4403C;
+  // Output Register (Changing the world)
   volatile uint32_t *gpio_out_reg = (volatile uint32_t *)0x3FF44004;
-  *gpio_out_reg ^= (1 << 2); 
-  for(volatile int i = 0; i < 500000; i++); 
+
+  // THEORY: Is Pin 4 HIGH? 
+  if (*gpio_in_reg & (1 << 4)) {
+      // Logic: If sensor is HIGH, blink/toggle LED
+      *gpio_out_reg ^= (1 << 2); 
+  } else {
+      // Logic: If sensor is LOW, force LED OFF (Emergency State)
+      *gpio_out_reg &= ~(1 << 2);
+  }
+
+  for(volatile int i = 0; i < 200000; i++); 
 }
